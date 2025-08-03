06a1d94aab2b58d731356b50b52524920d8044d3
Pierre Palatin <pierre@palatin.fr>
2025-08-03 23:04:04 +0200

    Get some stats on service points

--- generate.ts
+++ generate.ts
@@ -4,6 +4,7 @@ import { promises as fs } from "fs";
 import * as csv from "csv-parse/sync";
 import JSZip from "jszip";
 import * as datatypes from "./src/datatypes.js";
+import { count } from "console";
 
 const rawzip = await fs.readFile("actual_date-swiss-only-service_point-2025-08-02.csv.zip");
 const zip = await JSZip.loadAsync(rawzip);
@@ -18,9 +19,13 @@ const csvdata = await zipentry.async("string");
 const records = csv.parse(csvdata, { delimiter: ";", columns: true }) as datatypes.FullServicePoint[];
 
 const servicePoints: datatypes.ServicePoint[] = [];
+const countByType = new Map<datatypes.MeansOfTransport, number>();
 for (const r of records) {
     if (r.status != "VALIDATED") { continue; }
     if (!r.hasGeolocation) { continue; }
+
+    countByType.set(r.meansOfTransport, (countByType.get(r.meansOfTransport) ?? 0) + 1);
+
     if (r.meansOfTransport == "UNKNOWN" || r.meansOfTransport == "ELEVATOR" || r.meansOfTransport == "") { continue; }
 
     // For now, only have train stations to reduce size.
@@ -33,6 +38,9 @@ for (const r of records) {
     });
 }
 
+for (const mean of ([...countByType.keys()] as any).toSorted()) {
+    console.log(`${countByType.get(mean)} ${mean ?? "<empty>"} service points`);
+}
 console.log(`Total: ${records.length}, Selected: ${servicePoints.length}`);
 
 await fs.writeFile("src/extdata/servicepoints.json", JSON.stringify(servicePoints));
--- src/datatypes.ts
+++ src/datatypes.ts
@@ -8,10 +8,13 @@ export interface SwissMap extends TopoJSON.Topology {
 
 // https://opentransportdata.swiss/de/cookbook/masterdata-cookbook/servicepoints/
 // 3051;85;ch:1:sloid:3051;8503051;0;2021-04-01;9999-12-31;Zürich Binz;;ZBZ;true;true;true;ORDERLY;false;true;false;true;CH;Zürich;1;ZH;Zürich;112;Zürich;261;Zürich;;;TRAIN;;;true;true;8503051;;ch:1:sboid:100058;78;SZU;SZU;SZU;SZU;Sihltal-Zürich-Uetliberg-Bahn;Sihltal-Zürich-Uetliberg-Bahn;Sihltal-Zürich-Uetliberg-Bahn;Sihltal-Zürich-Uetliberg-Bahn;;2681558.27;1246329.425;8.51829736305;47.36276307407;421.6;2017-11-09 11:53:05;2024-04-08 09:26:06;VALIDATED
+
+export type MeansOfTransport = "TRAIN" | "BUS" | "TRAM" | "BOAT" | "CABLE_CAR" | "CHAIRLIFT" | "CABLE_RAILWAY" | "RACK_RAILWAY" | "METRO" | "ELEVATOR" | "UNKNOWN" | ""
+
 export type FullServicePoint = {
     designationOfficial: string,
     hasGeolocation: boolean,
-    meansOfTransport: "TRAIN" | "BUS" | "TRAM" | "BOAT" | "CABLE_CAR" | "CHAIRLIFT" | "CABLE_RAILWAY" | "RACK_RAILWAY" | "METRO" | "ELEVATOR" | "UNKNOWN" | "",
+    meansOfTransport: MeansOfTransport,
     wgs84East: string,
     wgs84North: string,
     status: "DRAFT" | "VALIDATED" | "IN_REVIEW" | "WITHDRAWN" | "REVOKED",
