8a3d7514ed408d9d194fad08a7669443af5985d0
Pierre Palatin <pierre@palatin.fr>
2025-08-06 22:55:59 +0200

    Filter for the day

--- src/gtfslib.ts
+++ src/gtfslib.ts
@@ -5,7 +5,7 @@
 
 import * as gtfs from "gtfs";
 
-function printAllRoutes(db) {
+function printAllRoutes(db: any, serviceIDs: Set<string>) {
     // Find all relevant stops.
     const rootStopIDs = new Set([
         "Parent8503011", // 'Zürich Wiedikon'
@@ -27,8 +27,9 @@ function printAllRoutes(db) {
     }
 
     const routeIDs = new Set<string>();
-    const trips = gtfs.getTrips({ "trip_id": Array.from(tripIDs) }, ["route_id"], [], { db: db });
+    const trips = gtfs.getTrips({ "trip_id": Array.from(tripIDs) }, ["route_id", "service_id"], [], { db: db });
     for (const t of trips) {
+        if (serviceIDs.size > 0 && !serviceIDs.has(t.service_id)) { continue; }
         routeIDs.add(t.route_id);
     }
 
@@ -53,10 +54,48 @@ function printAllRoutes(db) {
     }
 }
 
+type DayOfWeek = "monday" | "tuesday" | "wednesday" | "thursday" | "friday" | "saturday" | "sunday";
+
+function dayOfWeek(d: Date): DayOfWeek {
+    switch (d.getUTCDay()) {
+        case 0: return "sunday";
+        case 1: return "monday";
+        case 2: return "tuesday";
+        case 3: return "wednesday";
+        case 4: return "thursday";
+        case 5: return "friday";
+        case 6: return "saturday";
+    }
+    throw new Error(`invalid day of week ${d.getUTCDay()}`);
+}
+
 export async function run(gtfsDBFilename: string) {
     const db = gtfs.openDb({ sqlitePath: gtfsDBFilename });
 
-    printAllRoutes(db);
+    const targetDay = "2025-08-30";
+    const d = new Date(targetDay);
+    const gtfsDate = (d.getUTCFullYear() * 100 + d.getUTCMonth()) * 100 + d.getUTCDate();
+    const gtfsDayOfWeek = dayOfWeek(d);
+    console.log("gtfsDate", gtfsDate, "dayOfWeek", gtfsDayOfWeek);
+
+    // This does not include services which started the day before.
+
+    const relevantServiceIDs = new Set<string>();
+    const allCalendars = gtfs.getCalendars({}, [], [], { db: db });
+    for (const c of allCalendars) {
+        if (c.start_date > gtfsDate || c.end_date < gtfsDate) { continue; }
+        const v = c[gtfsDayOfWeek];
+        if (v === 0) { continue; }
+        relevantServiceIDs.add(c.service_id);
+    }
+
+    const calendarDates = gtfs.getCalendarDates({ date: gtfsDayOfWeek }, [], [], { db: db });
+    if (calendarDates.length > 0) {
+        console.log("found calendar_dates exception, not supported:", calendarDates);
+        throw new Error("not supported");
+    }
+
+    printAllRoutes(db, relevantServiceIDs);
 
     gtfs.closeDb(db);
 }
