fee3f03ac07c5da10986ea449d23dc0f408762c1
Pierre Palatin <pierre@palatin.fr>
2025-08-09 19:23:58 +0200

    Trying to get fitting trips in an sql query.
    Not there yet.

--- src/gtfslib.ts
+++ src/gtfslib.ts
@@ -10,6 +10,26 @@ function sqlNArgs(n: number): string {
     return "(" + Array(n).fill("?").join(",") + ")";
 }
 
+type DayOfWeek = "monday" | "tuesday" | "wednesday" | "thursday" | "friday" | "saturday" | "sunday";
+
+function dayOfWeek(d: Date): DayOfWeek {
+    switch (d.getUTCDay()) {
+        case 0: return "sunday";
+        case 1: return "monday";
+        case 2: return "tuesday";
+        case 3: return "wednesday";
+        case 4: return "thursday";
+        case 5: return "friday";
+        case 6: return "saturday";
+    }
+    throw new Error(`invalid day of week ${d.getUTCDay()}`);
+}
+
+// Transform a date object into a GTFS date number (e.g., 20250830)
+function convertDate(d: Date): number {
+    return (d.getUTCFullYear() * 100 + d.getUTCMonth()) * 100 + d.getUTCDate();
+}
+
 // Returns the list of unique stop names for the list of provided stop IDs.
 // This helps removing redundant stop names coming from list of platforms and the like.
 function listStopNames(db: sqlite.Database, stopIDs: string[]): string[] {
@@ -73,31 +93,11 @@ function findConnectedStops(db: sqlite.Database, stopIDs: string[]): string[] {
     return Array.from(expanded);
 }
 
-function findStopChildrenIDs(db: sqlite.Database, roots: Set<string>): Set<string> {
-    /*const stops = db.prepare(`
-        SELECT *
-        FROM stops
-        WHERE
-            stop_id IN ${sqlNArgs(roots.size)}
-            OR parent_station IN ${sqlNArgs(roots.size)}
-    `).all(Array.from(roots).concat(Array.from(roots)));*/
-
-
-    const stopIDs = new Set<string>();
-    const allStops = gtfs.getStops({}, ["parent_station", "stop_id"], [], { db: db });
-    for (const s of allStops) {
-        if (roots.has(s.parent_station ?? "") || roots.has(s.stop_id)) {
-            stopIDs.add(s.stop_id);
-        }
-    }
-    return stopIDs
-}
-
 function printAllRoutes(db: sqlite.Database, serviceIDs: Set<string>, minTime?: string, maxTime?: string) {
-    const stopIDs = findStopChildrenIDs(db, new Set([
+    const stopIDs = findConnectedStops(db, [
         "Parent8503011", // 'Zürich Wiedikon'
         "Parent8573710", // 'Zürich Wiedikon, Bahnhof'
-    ]));
+    ]);
 
     const tripIDs = new Set<string>();
     const stoptimes = gtfs.getStoptimes({ "stop_id": Array.from(stopIDs) }, ["trip_id", "arrival_time", "departure_time"], [], { db: db });
@@ -137,21 +137,6 @@ function printAllRoutes(db: sqlite.Database, serviceIDs: Set<string>, minTime?: 
     }
 }
 
-type DayOfWeek = "monday" | "tuesday" | "wednesday" | "thursday" | "friday" | "saturday" | "sunday";
-
-function dayOfWeek(d: Date): DayOfWeek {
-    switch (d.getUTCDay()) {
-        case 0: return "sunday";
-        case 1: return "monday";
-        case 2: return "tuesday";
-        case 3: return "wednesday";
-        case 4: return "thursday";
-        case 5: return "friday";
-        case 6: return "saturday";
-    }
-    throw new Error(`invalid day of week ${d.getUTCDay()}`);
-}
-
 function printLinesAtStop(db: sqlite.Database) {
     const targetDay = "2025-08-30";
     const d = new Date(targetDay);
@@ -186,7 +171,114 @@ export async function run(gtfsDBFilename: string) {
     // 'Zürich Wiedikon': "Parent8503011"
     // 'Zürich Wiedikon, Bahnhof': "Parent8573710"
 
-    console.log(listStopNames(db, findConnectedStops(db, ["Parent8503011"])));
+    const startIDs = findConnectedStops(db, ["Parent8503011"]);
+    console.log("Starts: ", listStopNames(db, startIDs).join(" | "));
+    const endIDs = findConnectedStops(db, ["Parent8502204"]);
+    console.log("Ends: ", listStopNames(db, endIDs).join(" | "));
+
+
+    const d = new Date("2025-08-30");
+    const gtfsDate = convertDate(d);
+    const gtfsDayOfWeek = dayOfWeek(d);
+    const minTime = "08:00:00";
+    const maxTime = "20:00:00";
+
+    const r = db.prepare(`
+        WITH
+            -- Services running that day.
+            in_range_services AS (
+                SELECT service_id FROM calendar
+                WHERE
+                    ${gtfsDayOfWeek} <> 0
+                    AND start_date <= @date
+                    AND end_date >= @date
+
+                UNION
+
+                SELECT service_id FROM calendar_dates
+                WHERE
+                    date = @date
+                    AND exception_type = 1
+                GROUP BY
+                    service_ID
+            ),
+            -- Select potential departure times for the start station.
+            start_times AS (
+                SELECT
+                    trip_id,
+                    stop_id,
+                    departure_time
+                FROM stop_times
+                WHERE
+                    departure_time >= @mintime
+                    AND stop_id IN ${sqlNArgs(startIDs.length)}
+            ),
+            -- Select potential stop times for the end station.
+            end_times AS (
+                SELECT
+                    trip_id,
+                    stop_id,
+                    arrival_time
+                FROM stop_times
+                WHERE
+                    arrival_time <= @maxtime
+                    AND stop_id IN ${sqlNArgs(endIDs.length)}
+            ),
+            -- Trips IDs
+            matching_trip_ids AS (
+                SELECT
+                    trip_id,
+                    departure_time,
+                    arrival_time,
+                    start_times.stop_id AS start_id,
+                    end_times.stop_id AS end_id
+                FROM start_times JOIN end_times USING (trip_id)
+            ),
+            -- Actual trips
+            matching_trips AS (
+                SELECT
+                    trip_id,
+                    route_id,
+                    departure_time,
+                    arrival_time,
+                    start_id,
+                    end_id
+                FROM
+                    matching_trip_ids
+                    JOIN trips USING (trip_id)
+                    JOIN in_range_services USING (service_id)
+
+            ),
+            -- Enrich with route names
+            with_details AS (
+                SELECT
+                    trip_id,
+                    route_id,
+                    departure_time,
+                    arrival_time,
+                    start_id,
+                    end_id,
+                    route_short_name,
+                    start_stops.stop_name as start_name,
+                    end_stops.stop_name as end_name
+                FROM
+                    matching_trips
+                    JOIN routes USING (route_id)
+                    JOIN stops AS start_stops ON (start_stops.stop_id = start_id)
+                    JOIN stops AS end_stops ON (end_stops.stop_id = end_id)
+            )
+        SELECT
+            *
+        FROM
+            with_details
+        ORDER BY departure_time
+        ;
+    `).all({
+        "date": gtfsDate,
+        "mintime": minTime,
+        "maxtime": maxTime,
+    }, startIDs, endIDs);
+    console.log(r);
 
     gtfs.closeDb(db);
 }
