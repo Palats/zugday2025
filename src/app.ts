f8005c140e447f3df665ee70271e3a8101f13c1a
Pierre Palatin <pierre@palatin.fr>
2025-08-10 11:08:16 +0200

    Prepare for a menu

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import { LitElement, css, html } from "lit"
-import { customElement } from "lit/decorators.js"
+import { LitElement, css, html, nothing } from "lit"
+import { customElement, state } from "lit/decorators.js"
 import "@material-symbols/font-400/outlined.css";
 import * as common from "./common";
 
@@ -10,8 +10,26 @@ import "./map";
  */
 @customElement("zg-app")
 export class ZGApp extends LitElement {
+  @state() private showMenu = false;
+
   render() {
-    return html`<zg-map></zg-map>`;
+    return html`
+      <div class="menubutton">
+        <button class="menubutton" @click=${() => { this.showMenu = !this.showMenu }}>
+          ${this.showMenu ? html`
+          <span class="material-symbols-outlined" title="Close menu">close</span>
+          `: html`
+          <span class="material-symbols-outlined" title="Open menu">menu</span>
+          `}
+        </button>
+      </div>
+      ${this.showMenu ? html`
+        <div class="menu">
+          Plop
+        </div>
+      `: nothing}
+      <zg-map > </zg-map>
+  `;
   }
 
   static styles = [common.sharedCSS, css`
@@ -20,8 +38,23 @@ export class ZGApp extends LitElement {
         height: 100%;
         display: flex;
         align-items: stretch;
-      }
-  `];
+    }
+
+    .menubutton {
+      position: fixed;
+      z-index: 1;
+
+      font-size: 24px;
+      margin-right: 10px;
+    }
+
+    .menu {
+      position: fixed;
+      width: 100px;
+      height: 100%;
+      background-color: #eeeeee;
+    }
+`];
 }
 
 declare global {
--- src/common.ts
+++ src/common.ts
@@ -2,25 +2,36 @@ import { css } from "lit"
 
 export const sharedCSS = css`
     .material-symbols-outlined {
-    font-family: 'Material Symbols Outlined';
-    font-weight: normal;
-    font-style: normal;
+        font-family: 'Material Symbols Outlined';
+        font-weight: normal;
+        font-style: normal;
 
-    line-height: 1;
-    letter-spacing: normal;
-    text-transform: none;
-    display: inline-block;
-    white-space: nowrap;
-    word-wrap: normal;
-    direction: ltr;
-    -webkit-font-feature-settings: 'liga';
-    -webkit-font-smoothing: antialiased;
+        line-height: 1;
+        letter-spacing: normal;
+        text-transform: none;
+        display: inline-block;
+        white-space: nowrap;
+        word-wrap: normal;
+        direction: ltr;
+        -webkit-font-feature-settings: 'liga';
+        -webkit-font-smoothing: antialiased;
 
-    font-feature-settings: 'liga';
-    vertical-align: text-bottom;
+        font-feature-settings: 'liga';
+        vertical-align: text-bottom;
     }
 
     .symbol-filled {
-    font-variation-settings: 'FILL' 1;
+        font-variation-settings: 'FILL' 1;
+    }
+
+    button {
+        border-radius: 3px;
+        margin: 1px;
+        border-style: outset;
+        border-width: 1px;
+    }
+
+    .hidden {
+        visibility: hidden;
     }
 `;
